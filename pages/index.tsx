29432a9b3b7c20de338b00a5eeb9509bf1b70955
Williams25 <william.souza.timao@hotmail.com>
2021-05-19 14:13:56 -0300

    retirando arrays, e adicioando component sellerslist e importando data.json para passar as props dos components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import { Carousel } from "../src/components";
+import { Carousel, SellersList } from "../src/components";
 import { Container } from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-
-type CarouselItem = {
-  source: string;
-  description: string;
-  link: string;
-};
+import data from "../data.json";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,29 +16,19 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Home() {
   const styled = useStyles();
-
-  const array: CarouselItem[] = [
-    {
-      source: "./images/2.jpg",
-      description: "sucos e doces villa piva",
-      link: "",
-    },
-    {
-      source: "./images/freshmania.png",
-      description: "caminhão de frete fresh mania",
-      link: "",
-    },
-    {
-      source: "./images/leite.png",
-      description: "leite letti A2",
-      link: "",
-    },
-  ];
-
+  const { carouselItems, sellersItems } = data;
   return (
     <>
       <Container className={styled.container}>
-        <Carousel carouselItemsDesktop={array} />
+        <Carousel
+          carouselItemsMobile={carouselItems}
+          carouselItemsDesktop={carouselItems}
+          slideDelay={3000}
+        />
+      </Container>
+
+      <Container className={styled.container}>
+        <SellersList title="Compre por loja" sellers={sellersItems} />
       </Container>
     </>
   );
