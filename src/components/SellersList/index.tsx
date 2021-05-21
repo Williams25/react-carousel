eef514f17d856fd92e1f807c6d320d4ccacd147f
Williams25 <william.souza.timao@hotmail.com>
2021-05-21 20:54:15 -0300

    ajuste no components sellerlist

--- src/components/SellersList/index.tsx
+++ src/components/SellersList/index.tsx
@@ -1,9 +1,11 @@
 import { Box, makeStyles, Theme, Typography, Grid } from "@material-ui/core";
+import { SellerButton } from "./SellerButton";
+import { useStyles } from "./styles";
 
 type SellersItem = {
-  url: string;
+  imgSource: string;
   name: string;
-  link: string;
+  hrefLink: string;
 };
 
 type SellersListProps = {
@@ -11,79 +13,26 @@ type SellersListProps = {
   sellers?: Array<SellersItem>;
 };
 
-const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    textAlign: "center",
-    marginBottom: "1rem",
-    width: "100%",
-    height: "auto",
-    zIndex: 2,
-  },
-  content: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    width: "75%",
-    height: "auto",
-    zIndex: 2,
-  },
-  imageItem: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    flexDirection: "column",
-    flexWrap: "nowrap",
-    margin: "1.5rem 1.5rem 0 1.5rem",
-    zIndex: 2,
-  },
-  image: {
-    width: "6rem",
-    height: "6rem",
-    zIndex: 2,
-  },
-}));
-
 export const SellersList = ({ title, sellers }: SellersListProps) => {
   const styled = useStyles();
   return (
     <>
-      <Box className={styled.container}>
-        <Typography variant="h2" component="span" style={{zIndex: 2}}>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Typography variant="h2" component="span" style={{ zIndex: 2 }}>
           {title}
         </Typography>
 
-        <div className={styled.content}>
-          {sellers.map((item) => {
-            return (
-              <Grid
-                container
-                item
-                xs
-                className={styled.imageItem}
-                key={item.name}
-              >
-                <a href={item.link}>
-                  <img
-                    className={styled.image}
-                    src={item.url}
-                    alt={item.name}
-                  />
-                  <Typography variant="subtitle2" component="p">
-                    {item.name}
-                  </Typography>
-                </a>
-              </Grid>
-            );
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          className={styled.content}
+        >
+          {sellers.map((item, index) => {
+            return <SellerButton sellerItem={item} key={index} />;
           })}
-        </div>
-      </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
