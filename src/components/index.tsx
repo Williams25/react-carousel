8ab2d5008b898e21d12d5c076dac0a83cbdc5598
Williams25 <william.souza.timao@hotmail.com>
2021-05-21 20:54:30 -0300

    exportando components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
 export { CarouselTextImage as Carousel } from "./Carousel";
 export { SellersList } from "./SellersList";
+export { FilterList } from "./FilterList";
+export { FilterPill } from "./FilterList/FilterPill";
